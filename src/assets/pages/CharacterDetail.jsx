33671c66f2e716cbc868819f111bee8fd1428e55
marco-rubio-bustos <marco.rubio.bustos@gmail.com>
2025-08-08 18:57:07 -0400

    redirigiendo al home al no encontrar personaje seleccionado

--- src/assets/pages/CharacterDetail.jsx
+++ src/assets/pages/CharacterDetail.jsx
@@ -1,54 +1,62 @@
-import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const CharacterDetail = () => {
   const location = useLocation();
-  const character = location.state.character;
+  const navigate = useNavigate();
+  const character = location.state?.character;
+
+  useEffect(() => {
+    if (!character) {
+      navigate("/");
+    }
+  }, [character, navigate]);
 
   return (
     <div className="card mb-3" style={{ maxWidth: "100%" }}>
       <div className="row g-0">
         <div className="col-md-4">
           <img
-            src={character.image}
+            src={character?.image}
             className="img-fluid rounded-start"
-            alt={character.name}
+            alt={character?.name}
           />
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="card-title">{character.name}</h5>
+            <h5 className="card-title">{character?.name}</h5>
             <p className="card-text m-0">
               <small className="text-body-secondary">
                 <b>Estado: </b>
-                {character.status}
+                {character?.status}
               </small>
             </p>
             <p className="card-text m-0">
               <small className="text-body-secondary">
                 <b>Género: </b>
-                {character.gender}
+                {character?.gender}
               </small>
             </p>
             <p className="card-text m-0">
               <small className="text-body-secondary">
                 <b>Especie: </b>
-                {character.species}
+                {character?.species}
               </small>
             </p>
             <p className="card-text m-0">
               <small className="text-body-secondary">
                 <b>Origen: </b>
-                {character.origin.name}
+                {character?.origin.name}
               </small>
             </p>
             <p className="card-text m-0">
               <small className="text-body-secondary">
-                <b>Ubicación: </b> {character.location.name}
+                <b>Ubicación: </b> {character?.location.name}
               </small>
             </p>
             <p className="card-text m-0">
               <small className="text-body-secondary">
-                <b>Cantidad de episodios: </b> {character.episode.length}
+                <b>Cantidad de episodios: </b> {character?.episode.length}
               </small>
             </p>
           </div>
